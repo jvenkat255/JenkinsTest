ad2e5315ef5ac16482d853f523f47bec2063cf0e
jvenkat255 <39788500+jvenkat255@users.noreply.github.com>
2019-03-25 10:46:29 -0500

    Update Sonarqube.groovy

--- Sonarqube.groovy
+++ Sonarqube.groovy
@@ -1,3 +1,6 @@
+import jenkins.model.*
+jenkins = Jenkins.instance
+
 def sonarQualityGateCheck() {
      timeout(time: 15, unit: 'MINUTES') {
         def qualityGate = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
