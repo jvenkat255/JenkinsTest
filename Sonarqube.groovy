f331f5ed807fd495ef3d9bae66fceceb4c1b48ae
jvenkat255 <39788500+jvenkat255@users.noreply.github.com>
2019-03-25 10:35:51 -0500

    Create Sonarqube.groovy

---
+++ Sonarqube.groovy
@@ -0,0 +1,9 @@
+def sonarQualityGateCheck() {
+     timeout(time: 15, unit: 'MINUTES') {
+        def qualityGate = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
+        if (qualityGate.status != 'OK') {
+            failedIn = "sonar quality gate check"
+            error("Pipeline aborted due to quality gate failure: ${qualityGate.status}")
+        }
+     }
+}
